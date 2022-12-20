933da0d45e6c1292102e0204f8eedbc04d6b50e0
Александр Джуманов <killer260598@mail.ru>
2022-12-20 16:48:26 +0300

    Added logging

--- kittybot.py
+++ kittybot.py
@@ -1,45 +1,55 @@
-import requests
-from telegram import ReplyKeyboardMarkup
-from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 import os
-from dotenv import load_dotenv 
+import logging
+import requests
+from dotenv import load_dotenv
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import CommandHandler, Updater
 
 load_dotenv()
 secret_token = os.getenv('TOKEN')
 
-updater = Updater(token=secret_token)
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO)
+
+
 URL_CAT = 'https://api.thecatapi.com/v1/images/search'
 URL_DOG = 'https://api.thedogapi.com/v1/images/search'
+
 
 def get_new_cat_image():
     try:
         response = requests.get(URL_CAT)
     except Exception as error:
-        print(error)      
+        logging.error(f'Ошибка при запросе к основному API cat: {error}')
         response = requests.get(URL_DOG)
-    
+
     response = response.json()
     random_cat = response[0].get('url')
-    return random_cat 
+    return random_cat
+
 
 def new_cat(update, context):
     chat = update.effective_chat
     context.bot.send_photo(chat.id, get_new_cat_image())
 
+
 def get_new_dog_image():
     try:
         response = requests.get(URL_DOG)
     except Exception as error:
-        print(error)      
+        logging.error(f'Ошибка при запросе к основному API dog: {error}')
         response = requests.get(URL_CAT)
-    
+
     response = response.json()
     random_dog = response[0].get('url')
-    return random_dog 
+    return random_dog
+
 
 def new_dog(update, context):
     chat = update.effective_chat
     context.bot.send_photo(chat.id, get_new_dog_image())
+
 
 def wake_up(update, context):
     chat = update.effective_chat
@@ -56,9 +66,16 @@ def wake_up(update, context):
     context.bot.send_photo(chat.id, get_new_cat_image())
     context.bot.send_photo(chat.id, get_new_dog_image())
 
-updater.dispatcher.add_handler(CommandHandler('start', wake_up))
-updater.dispatcher.add_handler(CommandHandler('cat', new_cat))
-updater.dispatcher.add_handler(CommandHandler('newdog', new_dog))
 
-updater.start_polling()
-updater.idle()
+def main():
+    updater = Updater(token=secret_token)
+    updater.dispatcher.add_handler(CommandHandler('start', wake_up))
+    updater.dispatcher.add_handler(CommandHandler('cat', new_cat))
+    updater.dispatcher.add_handler(CommandHandler('newdog', new_dog))
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
